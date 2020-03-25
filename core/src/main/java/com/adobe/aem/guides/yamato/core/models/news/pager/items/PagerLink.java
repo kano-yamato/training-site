8f08e4b5defb0423d5b40e9c68b1f70e2a3951c2
adobe kano <yuki.kano@adobe.com>
2020-03-25 14:46:40 +0900

    ページャー部分のHTMLを修正

--- core/src/main/java/com/adobe/aem/guides/yamato/core/models/news/pager/items/PagerLink.java
+++ core/src/main/java/com/adobe/aem/guides/yamato/core/models/news/pager/items/PagerLink.java
@@ -1,14 +1,31 @@
 package com.adobe.aem.guides.yamato.core.models.news.pager.items;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PagerLink {
     private final String tag_;
     private final String path_;
     private final int count_;
+    private final String currentAttribute_;
+    private Map<String, String> ariaCurrent_ = new HashMap<String, String>();
+    private Map<String, String> title_ = new HashMap<String, String>();
 
     public PagerLink(int currentPageNumber, int count) {
         count_ = count;
         tag_ = (currentPageNumber == count) ? "span" : "a";
-        path_ = "/content/yamato/jp/ja/news." + String.valueOf(count) + ".html";
+        path_ = (currentPageNumber == count) ? "" : "/content/yamato/jp/ja/news." + String.valueOf(count) + ".html";
+        currentAttribute_ = (currentPageNumber == count) ? "current" : "";
+        ariaCurrent_.put("aria-current", ariaCurrentValue(currentPageNumber, count));
+        title_.put("title", titlePageNumber(currentPageNumber, count));
+    }
+
+    private String ariaCurrentValue(int currentPageNumber, int count) {
+        return (currentPageNumber == count) ? "page" : "";
+    }
+
+    private String titlePageNumber(int currentPageNumber, int count) {
+        return (currentPageNumber == count) ? "" : "Page " + String.valueOf(count);
     }
 
     public String getTag() {
@@ -23,4 +40,15 @@ public class PagerLink {
         return count_;
     }
 
+    public String getCurrentAttribute() {
+        return currentAttribute_;
+    }
+
+    public Map<String, String> getAriaCurrent() {
+        return ariaCurrent_;
+    }
+
+    public Map<String, String> getTitle() {
+        return title_;
+    }
 }
--- core/src/test/java/com/adobe/aem/guides/yamato/core/models/news/pager/items/PagerLinkTest.java
+++ core/src/test/java/com/adobe/aem/guides/yamato/core/models/news/pager/items/PagerLinkTest.java
@@ -2,26 +2,77 @@ package com.adobe.aem.guides.yamato.core.models.news.pager.items;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+
 import org.junit.jupiter.api.Test;
 
 public class PagerLinkTest {
     @Test
-    public void testPagerLinkSpanTag() {
+    public void testGetTag_currentIsTrue() {
         assertEquals("span", new PagerLink(1, 1).getTag());
     }
 
     @Test
-    public void testPagerLinkATag() {
+    public void testGetTag_currentIsFalse() {
         assertEquals("a", new PagerLink(1, 2).getTag());
     }
 
     @Test
-    public void testPagerLinkCount() {
+    public void testGetCount() {
         assertEquals(2, new PagerLink(2, 2).getCount());
     }
 
     @Test
-    public void testPagerLinkPath() {
-        assertEquals("/content/yamato/jp/ja/news.1.html", new PagerLink(1, 1).getPath());
+    public void testGetPath_hrefIsEmpty() {
+        assertEquals("", new PagerLink(1, 1).getPath());
+    }
+
+    @Test
+    public void testGetPath_hrefIsNotEmpty() {
+        assertEquals("/content/yamato/jp/ja/news.2.html", new PagerLink(1, 2).getPath());
+    }
+
+    @Test
+    public void testGetCurrentAttr_currentIsTrue() {
+        assertEquals("current", new PagerLink(1, 1).getCurrentAttribute());
+    }
+
+    @Test
+    public void testGetCurrentAttr_currentIsFalse() {
+        assertEquals("", new PagerLink(1, 2).getCurrentAttribute());
+    }
+
+    @Test
+    public void testGetAriaCurrent_currentIsTrue() {
+        Map<String, String> expected = new ImmutableMap.Builder<String, String>()
+                .put("aria-current", "page").build();
+        Map<String, String> actual = new PagerLink(1, 1).getAriaCurrent();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetAriaCurrent_currentIsFalse() {
+        Map<String, String> expected = new ImmutableMap.Builder<String, String>()
+                .put("aria-current", "").build();
+        Map<String, String> actual = new PagerLink(1, 2).getAriaCurrent();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetTitle_currentIsTrue() {
+        Map<String, String> expected = new ImmutableMap.Builder<String, String>()
+                .put("title", "").build();
+        Map<String, String> actual = new PagerLink(1, 1).getTitle();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetTitle_currentIsFalse() {
+        Map<String, String> expected = new ImmutableMap.Builder<String, String>()
+                .put("title", "Page 2").build();
+        Map<String, String> actual = new PagerLink(1, 2).getTitle();
+        assertEquals(expected, actual);
     }
 }
