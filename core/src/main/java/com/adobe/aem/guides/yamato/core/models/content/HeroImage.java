2dce68c5af416b9e6554ca876b615bf68919e4d8
adobe kano <yuki.kano@adobe.com>
2020-04-16 13:43:24 +0900

    Sling Models を追加

---
+++ core/src/main/java/com/adobe/aem/guides/yamato/core/models/content/HeroImage.java
@@ -0,0 +1,33 @@
+package com.adobe.aem.guides.yamato.core.models.content;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(
+    adaptables = {SlingHttpServletRequest.class},
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+public class HeroImage {
+    @ValueMapValue
+    private List<String> items;
+
+    private List<Image> heroImages = new ArrayList<Image>();
+
+    @PostConstruct
+    public void init() {
+        items.stream().forEach(item -> {
+            heroImages.add(new Image(item));
+        });
+    }
+
+    public List<Image> getHeroImages() {
+        return heroImages;
+    }
+}
